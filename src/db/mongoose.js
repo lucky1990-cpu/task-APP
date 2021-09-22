cd9845b7fd0275915b7ac73a191ddc1e50d67b11
Kumar <lucky.a.kumar@accenture.com>
2021-09-22 12:45:52 +0530

    Init commir

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose =  require('mongoose')
 const validator = require('validator')
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true
 })
 //Create DB with name Users
